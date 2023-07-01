83c35ebed90df3bf69ee8b10dfb9388d519df849
seungjaejeon <wjstmdwo2000@naver.com>
2023-07-02 04:44:19 +0900

    item search

---
+++ src/main/java/hello/itemservice/repository/ItemRepository.java
@@ -0,0 +1,38 @@
+package hello.itemservice.repository;
+
+import hello.itemservice.domain.Item;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//@Repository -> component 스캔의 대상이 됨
+@Repository
+public class ItemRepository {
+    private static final Map<Long, Item> store = new HashMap<>(); //static 사용
+    private static long sequence = 0L; //static 사용
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+    public Item findById(Long id){
+        return store.get(id);
+    }
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    //    public void update(Item updateParam) {
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = store.get(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+    public void clearStore(){
+        store.clear();
+    }
+}
---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,40 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    public BasicItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("item1", 5000, 1));
+        itemRepository.save(new Item("item2", 10000, 2));
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+}
