09391e3ee8002dc66b7b85ec915ad99c5b5a8f2e
seungjaejeon <wjstmdwo2000@naver.com>
2023-07-02 06:03:31 +0900

    edit item, add item refactoring

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -40,16 +41,27 @@ public class BasicItemController {
 
     @GetMapping("/add")
     public String addForm(Model model) {
-        List<Item> items = itemRepository.findAll();
-        Item lastItem = items.get(items.size()-1);
-        model.addAttribute("item", lastItem);
         return "basic/addForm";
     }
 
     @PostMapping("/add")
-    public String save(@ModelAttribute Item item) {
-        itemRepository.save(item);
-        return "basic/item";
+    public String save(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
 }
